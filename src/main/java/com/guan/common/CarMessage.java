4840e74d66dd85f8bae8ec6b9c40b46722853760
ShepherdPro <tian-900923@163.com>
2020-06-28 12:01:34 +0800

    The Message Object

---
+++ src/main/java/com/guan/common/CarMessage.java
@@ -0,0 +1,10 @@
+package com.guan.common;
+
+public class CarMessage {
+    public static final String MESSAGE_CAR_PARK_PARAMETERS_ERROR = "Parameters Error while creating CarPark instance.";
+    public static final String MESSAGE_CAR_IMPL_PARAMETERS_ERROR = "Parameters Error while creating car instance.";
+    public static final String MESSAGE_STEPS_VALUE_NEGATIVE = "Steps value can not be negative.";
+    public static final String MESSAGE_TIMES_VALUE_NEGATIVE = "Times value can not be negative.";
+    public static final String MESSAGE_CAR_OUT_OF_BOUNDARIES = "Car moves outside the car park boundaries.";
+
+}
